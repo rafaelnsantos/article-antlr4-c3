c3f163e39417e690eb94e6a2bc42bf59853930a1
Rafael Santos <rafaelnsantos1@gmail.com>
2022-05-21 14:48:31 -0300

    eslint

--- tests/suggestions.basic.test.ts
+++ tests/suggestions.basic.test.ts
@@ -1,34 +1,38 @@
-import {expect} from "chai";
+import { expect } from 'chai';
 
-import { computeTokenPosition, getSuggestions, setTokenMatcher, filterTokens, filterTokens_fuzzySearch } from "../src";
+import {
+  computeTokenPosition,
+  getSuggestions,
+  setTokenMatcher,
+  filterTokens,
+  filterTokens_fuzzySearch,
+} from '../src';
 
-const suite = function() {
-    it("are suggested",
-        function() {
-            const code = `fun test() {
+const suite = function () {
+  it('are suggested', function () {
+    const code = `fun test() {
     try {
         doSomething()
     } 
 }`;
-            let suggestions = getSuggestions(code, { line: 4, column: 7 }, computeTokenPosition);
-            expect(suggestions.length).to.equal(51);
-        });
-    it("are suggested with partial match",
-        function() {
-            const code = `fun test() {
+    const suggestions = getSuggestions(code, { line: 4, column: 7 }, computeTokenPosition);
+    expect(suggestions.length).to.equal(51);
+  });
+  it('are suggested with partial match', function () {
+    const code = `fun test() {
     try {
         doSomething()
     } ca
 }`;
-            let suggestions = getSuggestions(code, { line: 4, column: 8 }, computeTokenPosition);
-            expect(suggestions.indexOf('catch')).to.be.greaterThan(-1);
-        });
+    const suggestions = getSuggestions(code, { line: 4, column: 8 }, computeTokenPosition);
+    expect(suggestions.indexOf('catch')).to.be.greaterThan(-1);
+  });
 };
 
 describe('Keywords', suite);
-describe('Keywords w/fuzzy completion', function() {
-    let oldMatcher = filterTokens;
-    beforeEach(() => setTokenMatcher(filterTokens_fuzzySearch));
-    suite();
-    afterEach(() => setTokenMatcher(oldMatcher));
+describe('Keywords w/fuzzy completion', function () {
+  const oldMatcher = filterTokens;
+  beforeEach(() => setTokenMatcher(filterTokens_fuzzySearch));
+  suite();
+  afterEach(() => setTokenMatcher(oldMatcher));
 });
